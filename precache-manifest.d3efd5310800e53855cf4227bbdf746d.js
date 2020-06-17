132fe75b86136858bb2ff734633a6fc8d6b73e4d
Milinda Ranawaka <49589047+MilindaRanawaka@users.noreply.github.com>
2020-06-17 14:09:35 +0530

    Updates

---
+++ precache-manifest.d3efd5310800e53855cf4227bbdf746d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a38ea72eb66da13d5ed78a308ca490",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "32a0faa7091f983dd0ca",
+    "url": "/me/static/css/main.7a86319e.chunk.css"
+  },
+  {
+    "revision": "c407a916f42a535c704a",
+    "url": "/me/static/js/2.46b95d03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/me/static/js/2.46b95d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32a0faa7091f983dd0ca",
+    "url": "/me/static/js/main.99837684.chunk.js"
+  },
+  {
+    "revision": "a6814c8c51bb554cdd51",
+    "url": "/me/static/js/runtime-main.de96f913.js"
+  },
+  {
+    "revision": "684d3de1d60d98a246fa09f256e23c45",
+    "url": "/me/static/media/logo.684d3de1.jpg"
+  }
+]);
